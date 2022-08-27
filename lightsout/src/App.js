001bc3121cb5bcb8f7fbb9efa03ea7e81ca35429
Finlay B <fb.code@proton.me>
2022-08-27 19:20:18 +0100

    Added attribution

--- lightsout/src/App.js
+++ lightsout/src/App.js
@@ -7,6 +7,11 @@ function App () {
     <div className='App'>
       <div className='App-container'>
         <div className='App-content'>
+          {/*
+              Logo provided by Reshot - https://www.reshot.com/
+              Accessed under Reshot Free License - https://www.reshot.com/license/
+              Image source - https://www.reshot.com/free-svg-icons/item/bohlam-6BPFM53A9S/
+            */}
           <img src={logo} className='App-logo' alt='logo' />
           <div className='App-header'>Lights Out</div>
           <Grid></Grid>
